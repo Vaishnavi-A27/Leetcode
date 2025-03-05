76843bdfe1202d6914b80a19df611da2fc90c11b
Vaishnavi-A27 <87163194+Vaishnavi-A27@users.noreply.github.com>
2025-03-05 13:36:10 +0530

    Time: 7 ms (63.57%), Space: 17.6 MB (88.8%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        
+        x=str(x)
+        y= str(x[::-1])
+
+        if x==y:
+            return True
+        else:
+            return False
